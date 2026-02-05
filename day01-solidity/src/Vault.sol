985eaca52db86e53bc0b72143dfb003988813b5c
noo-noo <noah.heurteaut@epitech.eu>
2026-02-05 11:55:44 +0100

    feat: add day03 step 03

--- day01-solidity/src/Vault.sol
+++ day01-solidity/src/Vault.sol
@@ -6,35 +6,29 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 using SafeERC20 for IERC20;
 
 contract Vault is Ownable, ReentrancyGuard {
-    string private _baseTokenURI;
-    uint256 private totalShares;
     IERC20 immutable asset;
-    
+    uint256 private totalShares;
     mapping(address => uint256) public sharesOf;
 
     event Deposit(address indexed user, uint256 assets, uint256 shares);
     event Withdraw(address indexed user, uint256 assets, uint256 shares);
+    event RewardAdded(uint256 ammont);
 
     error ZeroAmount();
     error InsufficientShares();
     error ZeroShares();
 
-    constructor(string memory baseURI_)
+    constructor(address _asset)
         Ownable(msg.sender)
     {
-        _baseTokenURI = baseURI_;
+        asset = IERC20(_asset);
         totalShares = 0;
     }
-
-    // modifier nonReentrant() {
-    //     require()
-    //     _;
-    // }
 
     function _convertToShares(uint256 assets) internal view returns (uint256)
     {
         if (totalShares == 0)
-            revert ZeroShares();
+            return assets;
         return ((assets * totalShares) / asset.balanceOf(address(this)));
     }
 
@@ -43,30 +37,31 @@ contract Vault is Ownable, ReentrancyGuard {
         return (shares * asset.balanceOf(address(this))) / totalShares;
     }
 
-    function deposit(uint256 assets) external returns (uint256 shares) {
+    function deposit(uint256 assets) external nonReentrant returns (uint256 shares) {
         if (assets == 0)
             revert ZeroAmount();
-        if (assets > 0 && _convertToShares(assets) > 0) {
-            sharesOf[msg.sender] += _convertToShares(assets);
-            totalShares += _convertToShares(assets);
-            asset.safeTransferFrom(msg.sender, address(this), assets);
-            emit Deposit(msg.sender, assets, _convertToShares(assets));
+        shares = _convertToShares(assets);
+        if (_convertToShares(assets) ==  0) {
+            revert InsufficientShares();
         }
-        return _convertToShares(assets);
+        sharesOf[msg.sender] += _convertToShares(assets);
+        totalShares += _convertToShares(assets);
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+        emit Deposit(msg.sender, assets, _convertToShares(assets));
     }
 
-    function withdraw(uint256 shares) public returns (uint256 assets) {
+    function withdraw(uint256 shares) public nonReentrant returns (uint256 assets) {
         if (shares == 0)
             revert ZeroAmount();
-        if (shares > totalShares)
+        assets = _convertToAssets(shares);
+        require(shares > totalShares, "ERROR OCCUR SHARES > TOTALSHARES");
+        if (sharesOf[msg.sender] < shares) {
             revert InsufficientShares();
-        if (shares > 0 && sharesOf[msg.sender] >= shares) {
-            sharesOf[msg.sender] -= shares;
-            totalShares -= shares;
-            asset.safeTransfer(address(this), shares);
-            emit Withdraw(msg.sender, _convertToAssets(shares), shares);
-            return _convertToAssets(shares);
         }
+        sharesOf[msg.sender] -= shares;
+        totalShares -= shares;
+        asset.safeTransfer(address(this), shares);
+        emit Withdraw(msg.sender, _convertToAssets(shares), shares);
     }
 
     function withdrawAll() public returns (uint256 assets)
@@ -83,5 +78,27 @@ contract Vault is Ownable, ReentrancyGuard {
     function previewWithdraw(uint256 shares) external view returns (uint256 assets)
     {
         return _convertToAssets(shares);
+    }
+
+    function totalAssets() public view returns (uint256 total_assets)
+    {
+        return asset.balanceOf(address(this));    
+    }
+
+    function currentRatio() external view returns (uint256 ratio) {
+        if (totalShares == 0)
+            return (1e18);
+        return (asset.balanceOf(address(this)) * 1e18) / totalShares;
+    }
+
+    function assetsOf(address user) external view returns (uint256 assets) {
+        return _convertToAssets(sharesOf[user]);
+    }
+
+    function addReward(uint256 amount) external onlyOwner nonReentrant {
+        if (amount > 0 && totalShares > 0) {
+            asset.safeTransferFrom(msg.sender, address(this), amount);
+            emit RewardAdded(amount);
+        }
     }
 }
